81d3c4eef1a88c1c8d8583406ef65bfca9af19c2
Alireza Chaychi <alireza.chaychi@gmail.com>
2021-11-17 15:30:12 -0800

    complimented functions so the tweet articles get created and get rendered dynamically

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,72 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function() {
+  const data = [
+    {
+      user: {
+        name: "Newton",
+        avatars: "https://i.imgur.com/73hZDYK.png",
+        handle: "@SirIsaac",
+      },
+      content: {
+        text:
+          "If I have seen further it is by standing on the shoulders of giants",
+      },
+      created_at: 1461116232227,
+    },
+    {
+      user: {
+        name: "Descartes",
+        avatars: "https://i.imgur.com/nlhLi3I.png",
+        handle: "@rd",
+      },
+      content: {
+        text: "Je pense , donc je suis",
+      },
+      created_at: 1461113959088,
+    },
+  ];
+  const createTweetElement = function(tweet) {
+    let $tweet = `<article class="tweet">
+          <header>
+            <img src="${tweet.user.avatars}" alt="avatar">
+            <span class="tweet-name">${tweet.user.name}</span>
+            <span>${tweet.user.handle}</span>
+          </header>
+          <p>${tweet.content.text}</p>
+          <footer>
+            <span>${timeago.format(tweet.created_at)} </span>
+            <span>
+              <i class="fas fa-heart"></i>
+              <i class="fas fa-flag"></i>
+              <i class="fas fa-retweet"></i>
+            </span>
+          </footer>
+        </article>`;
+    return $tweet;
+  };
+  // this function continusly renders tweets taken from a database
+  const renderTweet = function(tweets) {
+    for (const tweet of tweets) {
+      // here tweet taken from the array has turned into an article:
+      const readyTweet = createTweetElement(tweet);
+      // this is tweet appender:
+      $("#posted-tweets").prepend(readyTweet);
+    }
+  };
+
+  // here the rendertweet function is finally called
+  renderTweet(data);
+
+  $("form").on("submit", function(event) {
+    event.preventDefault();
+    console.log($(this).serialize());
+  });
+});
+// =================================Ajax functions ==================
+
+// $("form").submit(function(event) {
+//   console.log("submit prevented");
+//   event.preventDefault();
+// });
